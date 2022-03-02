b86040a9fe52509065fb723a83c8bd877aa5142e
IagoGoncalves <iago_375@hotmail.com>
2022-03-02 10:54:28 -0300

    pag-edital-concluida

--- wp-content/themes/consepe/archive-editais.php
+++ wp-content/themes/consepe/archive-editais.php
@@ -45,7 +45,7 @@ get_header(); ?>
                    </div>
                     <div class="tds-box">
                         <?php 
-                            $args = array('post_type' => 'editais','posts_per_page' => 4);
+                            $args = array('post_type' => 'editais','posts_per_page' => -1);
                             $var = new WP_Query($args);    
 
                             if($var->have_posts()):
@@ -53,19 +53,20 @@ get_header(); ?>
                                     $var->the_post(); ?>
                                         <!-- <a href="<?php the_permalink()?>">  -->
                                         <div class="box">
+                                            <div class="titulo-edital">
+                                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M18 0H6C4.9 0 4 0.9 4 2V14C4 15.1 4.9 16 6 16H18C19.1 16 20 15.1 20 14V2C20 0.9 19.1 0 18 0ZM9.5 7.5C9.5 8.33 8.83 9 8 9H7V11H5.5V5H8C8.83 5 9.5 5.67 9.5 6.5V7.5ZM14.5 9.5C14.5 10.33 13.83 11 13 11H10.5V5H13C13.83 5 14.5 5.67 14.5 6.5V9.5ZM18.5 6.5H17V7.5H18.5V9H17V11H15.5V5H18.5V6.5ZM7 7.5H8V6.5H7V7.5ZM2 4H0V18C0 19.1 0.9 20 2 20H16V18H2V4ZM12 9.5H13V6.5H12V9.5Z" fill="#112D4E"/>
+                                                </svg>
+                                                <span>Edital nº <?php the_title(); ?> - <?php the_time('d \d\e F \d\e Y') ?></span>
+                                            </div>
+
                                             <a href="">
-                                                <div class="titulo-data">
-                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M18 0H6C4.9 0 4 0.9 4 2V14C4 15.1 4.9 16 6 16H18C19.1 16 20 15.1 20 14V2C20 0.9 19.1 0 18 0ZM9.5 7.5C9.5 8.33 8.83 9 8 9H7V11H5.5V5H8C8.83 5 9.5 5.67 9.5 6.5V7.5ZM14.5 9.5C14.5 10.33 13.83 11 13 11H10.5V5H13C13.83 5 14.5 5.67 14.5 6.5V9.5ZM18.5 6.5H17V7.5H18.5V9H17V11H15.5V5H18.5V6.5ZM7 7.5H8V6.5H7V7.5ZM2 4H0V18C0 19.1 0.9 20 2 20H16V18H2V4ZM12 9.5H13V6.5H12V9.5Z" fill="#112D4E"/>
-                                                    </svg>
-                                                    <h4>Edital nº <?php the_title(); ?> - <?php the_date(); ?></h4>
-                                                </div>
-                                            <a href="">
-                                                <div class="botao">
-                                                    <span>Conferir</span>
-                                                    <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M2.00003 0L0.590027 1.41L5.17003 6L0.590027 10.59L2.00003 12L8.00003 6L2.00003 0Z" fill="#01244e"/>
-                                                    </svg>
+                                                <div class="down-edit">
+                                                    <span>fazer download do documento</span>
+                                                    <svg width="14" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<path d="M0 17.1458H14V15.1458H0V17.1458ZM14 6.14575H10V0.145752H4V6.14575H0L7 13.1458L14 6.14575Z" fill="white"/>
+													</svg>
+
                                                 </div>
                                             </a>  
                                         </div>  
@@ -73,13 +74,6 @@ get_header(); ?>
                                 endwhile;
                             endif;
                         wp_reset_postdata(); ?>
-                        <div class="ver-todos" >
-                            <a href="">Ver todos
-                                <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M2.00003 0L0.590027 1.41L5.17003 6L0.590027 10.59L2.00003 12L8.00003 6L2.00003 0Z" fill="#01244e"/>
-                                </svg>
-                            </a>
-                        </div>
                    </div>
                 </article>
             </div>            
